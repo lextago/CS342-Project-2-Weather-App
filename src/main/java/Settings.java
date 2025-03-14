1a8be0091b3ead732188562172d3fa7f5b09fb2f
Nick Laird <nicholaslaird7@gmail.com>
2025-03-14 11:06:54 -0500

    Update Settings with dropdowns and background

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -10,14 +10,12 @@ import javafx.scene.text.Font;
 
 public class Settings {
 	public static BorderPane getScreen() {
+		// Creates all text
+
 		Label settingsLabel = new Label("Settings");
 		settingsLabel.setTextFill(Color.rgb(0,0,0));
 		settingsLabel.setFont(new Font("Inter", 40));
 		settingsLabel.setPadding(new Insets(20,20,20,20));
-
-		TextField searchText = new TextField("Search");
-		searchText.setFont(new Font("Inter", 15));
-		searchText.setBackground(new Background(new BackgroundFill(Color.grayRgb(233), CornerRadii.EMPTY, Insets.EMPTY)));
 
 		Label textSizeText = new Label("Text Size");
 		textSizeText.setFont(new Font("Inter", 20));
@@ -28,13 +26,12 @@ public class Settings {
 		Label temperatureText = new Label("Temperature");
 		temperatureText.setFont(new Font("Inter", 20));
 
-		Label timeZoneText = new Label("Time Zone");
-		timeZoneText.setFont(new Font("Inter", 20));
-
 		Label chooseHourText = new Label("24hr/12hr");
 		chooseHourText.setFont(new Font("Inter", 20));
 
-		String fontSize[] = {"4px", "8px", "12px", "16px", "20px"};
+		// Creates all dropdowns
+
+		String fontSize[] = {"S", "M", "L"};
 		ComboBox fontSizeBox = new ComboBox(FXCollections.observableArrayList(fontSize));
 		fontSizeBox.setPrefWidth(100);
 
@@ -46,85 +43,77 @@ public class Settings {
 		ComboBox temperatureBox = new ComboBox(FXCollections.observableArrayList(temperatureTypes));
 		temperatureBox.setPrefWidth(100);
 
-		String timeZoneTypes[] = {"CST", "EST", "WST"};
-		ComboBox timeZoneBox = new ComboBox(FXCollections.observableArrayList(timeZoneTypes));
-		timeZoneBox.setPrefWidth(100);
-
-		String hourTypes[] = {"24hr", "12hr", "6hr"};
+		String hourTypes[] = {"24hr", "12hr"};
 		ComboBox hourBox = new ComboBox(FXCollections.observableArrayList(hourTypes));
 		hourBox.setPrefWidth(100);
 
-		HBox searchTextHBoxTemp = new HBox();
-		searchTextHBoxTemp.setStyle("-fx-background-color: #E9E9E9;");
-		searchTextHBoxTemp.getChildren().addAll(searchText);
-		searchTextHBoxTemp.setBorder(Border.stroke(Color.BLACK));
-		BorderPane settingSearchPanel = new BorderPane(searchTextHBoxTemp);
-		settingSearchPanel.setPadding(new Insets(5,20,20,25));
-		HBox searchTextHBox = new HBox(settingSearchPanel);
+		// Creates "Text Size" panel
 
-		HBox settingBoxOneHBoxTemp = new HBox();
-		settingBoxOneHBoxTemp.setPadding(new Insets(5, 5, 5, 5));
-		settingBoxOneHBoxTemp.setStyle("-fx-background-color: #FFFFFF;");
-		settingBoxOneHBoxTemp.getChildren().addAll(textSizeText, fontSizeBox);
-		settingBoxOneHBoxTemp.setAlignment(Pos.CENTER);
-		settingBoxOneHBoxTemp.setBorder(Border.stroke(Color.BLACK));
-		settingBoxOneHBoxTemp.setSpacing(132);
-		BorderPane settingPanelOne = new BorderPane(settingBoxOneHBoxTemp);
-		HBox settingBoxOneHBox= new HBox(settingPanelOne);
-		settingBoxOneHBox.setAlignment(Pos.CENTER);
+		BorderPane settingPanelText = new BorderPane();
+		settingPanelText.setLeft(textSizeText);
+		settingPanelText.setRight(fontSizeBox);
 
-		HBox settingBoxTwoHBoxTemp = new HBox();
-		settingBoxTwoHBoxTemp.setPadding(new Insets(5, 5, 5, 5));
-		settingBoxTwoHBoxTemp.setStyle("-fx-background-color: #FFFFFF;");
-		settingBoxTwoHBoxTemp.getChildren().addAll(themesText, themesBox);
-		settingBoxTwoHBoxTemp.setAlignment(Pos.CENTER);
-		settingBoxTwoHBoxTemp.setBorder(Border.stroke(Color.BLACK));
-		settingBoxTwoHBoxTemp.setSpacing(140);
-		BorderPane settingPanelTwo = new BorderPane(settingBoxTwoHBoxTemp);
-		HBox settingBoxTwoHBox= new HBox(settingPanelTwo);
-		settingBoxTwoHBox.setAlignment(Pos.CENTER);
+		settingPanelText.setPadding(new Insets(5));
+		settingPanelText.setBorder(Border.stroke(Color.BLACK));
+		settingPanelText.setStyle("-fx-background-color: #FFFFFF;");
+		settingPanelText.setPrefWidth(340);
 
-		HBox settingBoxThreeHBoxTemp = new HBox();
-		settingBoxThreeHBoxTemp.setPadding(new Insets(5, 5, 5, 5));
-		settingBoxThreeHBoxTemp.setStyle("-fx-background-color: #FFFFFF;");
-		settingBoxThreeHBoxTemp.getChildren().addAll(temperatureText, temperatureBox);
-		settingBoxThreeHBoxTemp.setAlignment(Pos.CENTER);
-		settingBoxThreeHBoxTemp.setBorder(Border.stroke(Color.BLACK));
-		settingBoxThreeHBoxTemp.setSpacing(100);
-		BorderPane settingPanelThree = new BorderPane(settingBoxThreeHBoxTemp);
-		HBox settingBoxThreeHBox = new HBox(settingPanelThree);
-		settingBoxThreeHBox.setAlignment(Pos.CENTER);
+		HBox settingBoxText = new HBox(settingPanelText);
+		settingBoxText.setAlignment(Pos.CENTER);
 
-		HBox settingBoxFourHBoxTemp = new HBox();
-		settingBoxFourHBoxTemp.setPadding(new Insets(5, 5, 5, 5));
-		settingBoxFourHBoxTemp.setStyle("-fx-background-color: #FFFFFF;");
-		settingBoxFourHBoxTemp.getChildren().addAll(timeZoneText, timeZoneBox);
-		settingBoxFourHBoxTemp.setAlignment(Pos.CENTER);
-		settingBoxFourHBoxTemp.setBorder(Border.stroke(Color.BLACK));
-		settingBoxFourHBoxTemp.setSpacing(118);
-		BorderPane settingPanelFour = new BorderPane(settingBoxFourHBoxTemp);
-		HBox settingBoxFourHBox = new HBox(settingPanelFour);
-		settingBoxFourHBox.setAlignment(Pos.CENTER);
+		// Creates "Themes" panel
 
-		HBox settingBoxFiveHBoxTemp = new HBox();
-		settingBoxFiveHBoxTemp.setPadding(new Insets(5, 5, 5, 5));
-		settingBoxFiveHBoxTemp.setStyle("-fx-background-color: #FFFFFF;");
-		settingBoxFiveHBoxTemp.getChildren().addAll(chooseHourText, hourBox);
-		settingBoxFiveHBoxTemp.setAlignment(Pos.CENTER);
-		settingBoxFiveHBoxTemp.setBorder(Border.stroke(Color.BLACK));
-		settingBoxFiveHBoxTemp.setSpacing(125);
-		BorderPane settingPanelFive = new BorderPane(settingBoxFiveHBoxTemp);
-		HBox settingBoxFiveHBox = new HBox(settingPanelFive);
-		settingBoxFiveHBox.setAlignment(Pos.CENTER);
+		BorderPane settingPanelThemes = new BorderPane();
+		settingPanelThemes.setLeft(themesText);
+		settingPanelThemes.setRight(themesBox);
 
-		VBox settingBoxOne = new VBox(4, settingsLabel, searchTextHBox);
-		VBox settingBoxTwo = new VBox(4, settingBoxOneHBox, settingBoxTwoHBox, settingBoxThreeHBox);
-		VBox settingBoxThree = new VBox(4, settingBoxFourHBox, settingBoxFiveHBox);
+		settingPanelThemes.setPadding(new Insets(5));
+		settingPanelThemes.setBorder(Border.stroke(Color.BLACK));
+		settingPanelThemes.setStyle("-fx-background-color: #FFFFFF;");
+		settingPanelThemes.setPrefWidth(340);
+
+		HBox settingBoxThemes = new HBox(settingPanelThemes);
+		settingBoxThemes.setAlignment(Pos.CENTER);
+
+		// Creates "Temperature" panel
+
+		BorderPane settingPanelTemperature = new BorderPane();
+		settingPanelTemperature.setLeft(temperatureText);
+		settingPanelTemperature.setRight(temperatureBox);
+
+		settingPanelTemperature.setPadding(new Insets(5));
+		settingPanelTemperature.setBorder(Border.stroke(Color.BLACK));
+		settingPanelTemperature.setStyle("-fx-background-color: #FFFFFF;");
+		settingPanelTemperature.setPrefWidth(340);
+
+		HBox settingBoxTemperature = new HBox(settingPanelTemperature);
+		settingBoxTemperature.setAlignment(Pos.CENTER);
+
+		// Creates "24hr/12hr" panel
+
+		BorderPane settingPanelHour = new BorderPane();
+		settingPanelHour.setLeft(chooseHourText);
+		settingPanelHour.setRight(hourBox);
+
+		settingPanelHour.setPadding(new Insets(5));
+		settingPanelHour.setBorder(Border.stroke(Color.BLACK));
+		settingPanelHour.setStyle("-fx-background-color: #FFFFFF;");
+		settingPanelHour.setPrefWidth(340);
+
+		HBox settingBoxHour = new HBox(settingPanelHour);
+		settingBoxHour.setAlignment(Pos.CENTER);
+
+		// Displays VBox in order from top to bottom of the screen
+
+		VBox settingBoxTop = new VBox(4, settingsLabel, settingBoxText, settingBoxThemes, settingBoxTemperature);
+		VBox settingBoxBottom = new VBox(4, settingBoxHour);
+
+		// Sets the background
 
 		BackgroundFill background_fill = new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY);
 		Background background = new Background(background_fill);
 
-		VBox root = new VBox(20, settingBoxOne, settingBoxTwo, settingBoxThree);
+		VBox root = new VBox(20, settingBoxTop, settingBoxBottom);
 		root.setBackground(background);
 
 		BorderPane borderPane = new BorderPane(root);
