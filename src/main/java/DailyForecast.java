a92eb8a38a8cc57b33976320028e65ea1999e9e9
Nick Laird <nicholaslaird7@gmail.com>
2025-03-16 23:19:24 -0500

    Update Daily Forecast

--- src/main/java/DailyForecast.java
+++ src/main/java/DailyForecast.java
@@ -1,7 +1,4 @@
 import hourlyWeather.HourlyPeriod;
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
@@ -10,71 +7,17 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.util.Pair;
 
-import javax.swing.*;
 import java.text.SimpleDateFormat;
-import java.time.format.TextStyle;
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class DailyForecast extends SceneBuilder{
 	public static BorderPane getScreen(){
 		//https://api.weather.gov/gridpoints/LOT/77,70/forecast/hourly
-
-		//Testing!!!
-		//
- 		//
-		// 1st hour
-
-		HourlyPeriod currentForecast = MyWeatherAPI.getHourlyForecast("LOT", 77, 70).get(0);
-		System.out.println("Forecast right now (current hour)");
-		System.out.println(currentForecast.shortForecast);
-
-		Date currentHour = currentForecast.startTime;//.after(new Date(Calendar.YEAR, Calendar.MARCH, Calendar.DAY_OF_MONTH)) ? currentForecast.startTime : new Date();
-		System.out.println("All details of forecast right now: (Current hour)");
-		System.out.println(currentHour);
-
-		SimpleDateFormat localDateFormat = new SimpleDateFormat("hh a");
-		System.out.println("Current time:");
-		System.out.println(localDateFormat.format(currentHour));
-
-
-        // 2nd hour
-		HourlyPeriod currentForecastTwo = MyWeatherAPI.getHourlyForecast("LOT", 77, 70).get(1);
-		System.out.println("\nForecast right now (2nd hour )");
-		System.out.println(currentForecastTwo.shortForecast);
-
-		Date currentHourTwo = currentForecastTwo.startTime;//.after(new Date(Calendar.YEAR, Calendar.MARCH, Calendar.DAY_OF_MONTH+1)) ? currentForecast.startTime : new Date();
-		System.out.println("All details of forecast right now: (2nd hour)");
-		System.out.println(currentHourTwo);
-
-		SimpleDateFormat localDateFormatTwo = new SimpleDateFormat("hh a");
-		System.out.println("Current time:");
-		System.out.println(localDateFormatTwo.format(currentHourTwo));
-
-		// Next Day
-		SimpleDateFormat localDateFormatMilitary = new SimpleDateFormat("HH");
-		System.out.println("\nCurrent time: (Military)");
-		System.out.println(localDateFormatMilitary.format(currentHour));
-		int current = Integer.parseInt(localDateFormatMilitary.format(currentHour));
-		int calculateNextDay = 24 - current;
-		System.out.println("Next day at: ");
-		System.out.println(calculateNextDay);
-
-		// Next Day Data Testing
-		HourlyPeriod currentForecastThree = MyWeatherAPI.getHourlyForecast("LOT", 77, 70).get(calculateNextDay); // Very important, gets to next day
-		Date currentHourThree = currentForecastThree.startTime;//.after(new Date(Calendar.YEAR, Calendar.MARCH, Calendar.DAY_OF_MONTH+1)) ? currentForecast.startTime : new Date();
-		System.out.println("\nAll details of forecast right now: (next day)");
-		System.out.println(currentHourThree);
-
-		//
- 		//
- 		// Testing!
-		// After clicking day button:
-		// Implement a loop here for daily based on amount of days chosen (3,5, or 7)(current day):
-		// print what day it is: "Sat March 14"
-		// print the hourly times.
-		// Print their forecasts.
 
 		VBox root = new VBox(5);
 
@@ -88,11 +31,34 @@ public class DailyForecast extends SceneBuilder{
 			int number = Integer.parseInt(day.split("")[0]);
 
 			textFieldsHolder.getChildren().clear();
+
+			int currents = 0;
+
+			List<HourlyPeriod> forecasts = MyWeatherAPI.getHourlyForecast("LOT", 77, 70);
+			ArrayList<Pair<String, double[]>> minAndMax = MyWeatherAPI.getMinAndMaxTemperatures("LOT", 77, 70);
+			SimpleDateFormat localDateFormatCurrentDays2 = new SimpleDateFormat("E MM/dd");
+			SimpleDateFormat localDateFormatCurrentDays = new SimpleDateFormat("HH"); // Calculates current hour
+
+
 			for (int i = 0; i < number; i++) {
-				TextField field = new TextField("Sat " + (i + 1) + "/" + (i+2));
+				HourlyPeriod currentForecastNows = forecasts.get(currents);
+				Date currentDays = currentForecastNows.startTime;
+				int currentTimes = Integer.parseInt(localDateFormatCurrentDays.format(currentDays));
+				if (i == 0) {
+					currents = 24 - currentTimes;
+				} else {
+					currents += 24;
+				}
+
+				String formattedDate = localDateFormatCurrentDays2.format(currentDays);
+				double min = minAndMax.get(i).getValue()[0];
+				double max = minAndMax.get(i).getValue()[1];
+				min = min*1.8 + 32; // Celsius to Fahrenheit
+				max = max*1.8 + 32;
+
+				TextField field = new TextField(formattedDate + " " + min + " " + max);
 				field.setEditable(false);
 				field.setFont(new Font("Inter", 20));
-				//field.setStyle("-fx-background-color: #588157;");
 				field.setStyle("-fx-text-fill: #ffffff; -fx-background-color: rgba(88,129,87,0.8);");
 				field.setBorder(Border.stroke(Color.BLACK));
 				field.setPrefSize(160,50);
