7352e2fac96ad44ddaad67ee3c9a30ce5e690046
Alexander Tago <alexetago@gmail.com>
2025-03-17 10:06:48 -0500

    worked on home

--- src/main/java/HomeScreen.java
+++ src/main/java/HomeScreen.java
@@ -21,85 +21,86 @@ import java.util.Date;
 public class HomeScreen extends SceneBuilder{
 	private static ArrayList<HourlyPeriod> hourlyForecast;
 
-	private static Button prevHourButton; //static global variable used for lambda method which requires 'final' variables
-	private static BorderPane prevHourBox;
+	private static BorderPane prevHourBox; //static global variable used for lambda method which requires 'final' variables
+	static DropShadow dropShadow = new DropShadow();
 
 	public static BorderPane getScreen(){
-		//int temp = WeatherAPI.getTodaysTemperature(77,70);
 		Period currentForecast = WeatherAPI.getForecast(region, gridX, gridY).get(0);
 		hourlyForecast = MyWeatherAPI.getHourlyForecast(region,gridX,gridY);
+//		double[] minAndMax = MyWeatherAPI.getMinAndMaxTemperature(region, gridX, gridY).get(0);
 		if (hourlyForecast == null){
 			System.out.println("No forecast found");
 		}
 
 		//Home screen is split into two main components: the top half and bottom half.
-		BorderPane homeBoxOne;
-		HBox homeBoxTwo;
-
 		//---elements for homeBoxOne (top portion of screen)
-		HBox homeBoxOneTop; //Placed at the very top of the screen
-		VBox homeBoxOneCenter; //Root for the top portion of the screen
-
 		Button locationButton = getLocationButton();
 
 		Label temperatureLabel = new Label(hourlyForecast.get(0).temperature + "°");
 		Label weatherLabel = new Label(hourlyForecast.get(0).shortForecast);
+		weatherLabel.setTextFill(Color.web("#FCFFCB"));
+		weatherLabel.setFont(Font.font("Verdana", 14));
+
 		temperatureLabel.setFont(Font.font("Verdana", FontWeight.BOLD,50));
 		temperatureLabel.setTextFill(Color.rgb(255,255,255));
+		temperatureLabel.setEffect(dropShadow);
 
-		homeBoxOneTop = new HBox(locationButton);
+		HBox homeBoxOneTop = new HBox(locationButton); //Placed at the very top of the screen
 		homeBoxOneTop.setAlignment(Pos.CENTER);
 
-		homeBoxOneCenter = new VBox(temperatureLabel, weatherLabel);
+		VBox homeBoxOneCenter = new VBox(temperatureLabel, weatherLabel); //Root for the top portion of the screen
 		homeBoxOneCenter.setAlignment(Pos.CENTER);
 
 		//--putting all the elements together for homeBoxOne (top portion of the screen)
-		homeBoxOne = new BorderPane(homeBoxOneCenter);
-		homeBoxOne.setPrefHeight(300);
+		BorderPane homeBoxOne = new BorderPane(homeBoxOneCenter);
+		homeBoxOne.setPrefHeight(150);
 		homeBoxOne.setTop(homeBoxOneTop);
 
+		//middle of homeboxone and two
+		Label descriptionText = new Label(currentForecast.detailedForecast);
+		descriptionText.setWrapText(true);
+		descriptionText.setFont(Font.font("Verdana", FontWeight.MEDIUM,12));
+		descriptionText.setTextFill(Color.rgb(255,255,255));
+		descriptionText.setEffect(dropShadow);
+
+		BorderPane descriptionBox = new BorderPane(descriptionText);
+		descriptionBox.setPrefSize(300, 70);
+		descriptionBox.setId("homeBox");
+		descriptionBox.setPadding(new Insets(10));
+
 		//---elements for homeBoxTwo (lower portion of screen)
-		VBox homeBoxTwoLeft, homeBoxTwoRight; //The bottom half of the screen is split into a left and right component
+		//The bottom half of the screen is split into a left and right component
 
 		ScrollPane hourlyForecastScroll = getHourlyScroll();
 
 		//---elements in homeBoxTwoLeft (left side of homeBoxTwo)
-		TextField todayWeatherText;
-		TextArea descriptionText;
-
-		todayWeatherText = new TextField("Today's Weather");
-		todayWeatherText.setEditable(false);
-		todayWeatherText.setFont(Font.font("Verdana", 14));
-		todayWeatherText.setMaxSize(220,18);
-
-		descriptionText = new TextArea(currentForecast.detailedForecast);
-		descriptionText.setEditable(false);
-		descriptionText.setMaxSize(220, 70);
-		descriptionText.setWrapText(true);
-		descriptionText.setFont(Font.font("Verdana", 10));
+		Label todayWeatherText = new Label("Today's Weather");
+		todayWeatherText.setFont(Font.font("Verdana",FontWeight.BOLD, 16));
+		todayWeatherText.setTextFill(Color.rgb(255,255,255));
+		todayWeatherText.setPrefSize(220,18);
+		todayWeatherText.setEffect(dropShadow);
 
 		//--putting all the elements together for the left part of the bottom of the screen
-		homeBoxTwoLeft = new VBox(todayWeatherText, descriptionText, hourlyForecastScroll);
+		VBox homeBoxTwoLeft = new VBox(todayWeatherText, hourlyForecastScroll);
 
 		//---elements in homeBoxTwoRight (right side of homeBoxTwo)
-		TextField alertsText, image;
-
-		alertsText = new TextField("Alerts");
+		TextField alertsText = new TextField("Alerts");
 		alertsText.setPrefSize(100, 200);
 		alertsText.setEditable(false);
 		alertsText.setAlignment(Pos.TOP_LEFT);
+		alertsText.setId("homeBox");
 
-		image = new TextField("image");
+		TextField image = new TextField("image");
 		image.setPrefSize(100, 100);
 
-		homeBoxTwoRight = new VBox(10, alertsText, image);
+		VBox homeBoxTwoRight = new VBox(10, alertsText, image);
 
 		//--putting all elements together for homeBoxTwo (bottom portion of screen)
-		homeBoxTwo = new HBox(10, homeBoxTwoLeft, homeBoxTwoRight);
+		HBox homeBoxTwo = new HBox(10, homeBoxTwoLeft, homeBoxTwoRight);
 		homeBoxTwo.setAlignment(Pos.CENTER);
 
 		//--putting all elements together in BorderPane root (whole screen)
-		VBox centerRoot = new VBox(homeBoxOne);
+		VBox centerRoot = new VBox(10, homeBoxOne, descriptionBox);
 
 		BorderPane root = new BorderPane(centerRoot);
 		root.setPadding(new Insets(10, 10, 0, 10));
@@ -143,6 +144,7 @@ public class HomeScreen extends SceneBuilder{
 		VBox hourlyForecastBox = new VBox();
 		ScrollPane hourlyForecastScroll = new ScrollPane(hourlyForecastBox); //This ScrollPane allows the user to scroll through the VBox
 		hourlyForecastScroll.setPrefSize(220, 300);
+		hourlyForecastScroll.setId("shelfScroll");
 
 		ArrayList<HBox> moreInfoBoxes = new ArrayList<>();
 		ArrayList<BorderPane> hourBoxes = new ArrayList<>();
@@ -153,8 +155,6 @@ public class HomeScreen extends SceneBuilder{
 			Date currentHour = currentPeriod.startTime;
 			SimpleDateFormat localDateFormat = new SimpleDateFormat("hh a");
 			String hourTime = localDateFormat.format(currentHour);
-
-			DropShadow dropShadow = new DropShadow();
 
 			Label time = new Label(hourTime);
 			time.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, 15));
@@ -226,6 +226,7 @@ public class HomeScreen extends SceneBuilder{
 			});
 		}
 
+		hourlyForecastScroll.setStyle("-fx-background-color: transparent");
 
 		return hourlyForecastScroll;
 	}
